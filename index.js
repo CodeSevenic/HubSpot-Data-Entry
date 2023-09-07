d40ca0cf6b920231f54f1933ce9bfb3af723681c
CodeSevenic <sevenicbookings@gmail.com>
2023-09-08 01:51:59 +0200

    PurpleWeb code update

--- index.js
+++ index.js
@@ -62,7 +62,7 @@ const logDuplicateCompanies = () => {
 // logDuplicateCompanies();
 
 // console.log('Contacts: ', contactsData);
-// console.log('Deals: ', dealsData);
+console.log('Deals: ', dealsData);
 
 // Function to validate email address
 // Function to validate email address
@@ -203,5 +203,5 @@ const runCreateContacts = async () => {
     await new Promise((resolve) => setTimeout(resolve, 100));
   }
 };
-runCreateContacts();
+// runCreateContacts();
 // runCreateDeals();
